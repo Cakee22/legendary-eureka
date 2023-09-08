920320f9ad01f1f3941ba1dd4a615dc3b187072b
Xinjie Zhang <2649573339@qq.com>
2023-09-08 15:55:47 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,10 +17,11 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
+    String language = args[5];
+    String country = args[5];
+    String name = args[5];
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
 }
+
